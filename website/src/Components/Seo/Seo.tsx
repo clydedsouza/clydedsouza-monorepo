58aedcc690e53b334290be19a33b59344f4ed95e
Clyde D'Souza <clyd94@gmail.com>
2023-06-11 00:02:51 +1200

    Replace react helmet with react-helmet-async

--- website/src/Components/Seo/Seo.tsx
+++ website/src/Components/Seo/Seo.tsx
@@ -1,4 +1,4 @@
-import { Helmet } from 'react-helmet'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { ISeo } from '../../Types/Seo'
 
 function Seo(props: ISeo) {
@@ -12,52 +12,54 @@ function Seo(props: ISeo) {
   }`
 
   return (
-    <Helmet
-      title={title}
-      defaultTitle="Clyde D'Souza – Software Engineer and Author"
-      htmlAttributes={{ lang: 'en' }}
-      meta={[
-        {
-          name: `description`,
-          content: description,
-        },
-        {
-          property: 'og:url',
-          content: url,
-        },
+    <HelmetProvider>
+      <Helmet
+        title={title}
+        defaultTitle="Clyde D'Souza – Software Engineer and Author"
+        htmlAttributes={{ lang: 'en' }}
+        meta={[
+          {
+            name: `description`,
+            content: description,
+          },
+          {
+            property: 'og:url',
+            content: url,
+          },
 
-        {
-          property: 'og:title',
-          content: title,
-        },
-        {
-          property: 'og:description',
-          content: description,
-        },
-        {
-          property: 'og:image',
-          content: image,
-        },
-        {
-          property: 'twitter:card',
-          content: description,
-        },
-        {
-          property: 'twitter:title',
-          content: title,
-        },
-        {
-          property: 'twitter:description',
-          content: description,
-        },
-        {
-          property: 'twitter:image',
-          content: image,
-        },
-      ]}
-    >
-      <link rel="canonical" href={url} />
-    </Helmet>
+          {
+            property: 'og:title',
+            content: title,
+          },
+          {
+            property: 'og:description',
+            content: description,
+          },
+          {
+            property: 'og:image',
+            content: image,
+          },
+          {
+            property: 'twitter:card',
+            content: description,
+          },
+          {
+            property: 'twitter:title',
+            content: title,
+          },
+          {
+            property: 'twitter:description',
+            content: description,
+          },
+          {
+            property: 'twitter:image',
+            content: image,
+          },
+        ]}
+      >
+        <link rel="canonical" href={url} />
+      </Helmet>
+    </HelmetProvider>
   )
 }
 
