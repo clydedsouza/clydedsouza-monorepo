5b1aa507d2004bc603c47b7f9442c0f812091e53
Clyde D'Souza <clyd94@gmail.com>
2023-06-11 01:03:52 +1200

    Cosmetic update

--- website/src/Components/Sidebar/Headline/Headline.tsx
+++ website/src/Components/Sidebar/Headline/Headline.tsx
@@ -3,7 +3,7 @@ import './Headline.scss'
 function Headline() {
   return (
     <div className="headline">
-      <h1>Clyde DSouza</h1>
+      <h1>Clyde D&apos;Souza</h1>
       <div>
         <p>Software Engineer and Author</p>
         <p>
--- website/src/Pages/AboutMe/AboutMe.tsx
+++ website/src/Pages/AboutMe/AboutMe.tsx
@@ -7,6 +7,7 @@ function AboutMe() {
     <>
       <Seo />
       <Bio />
+      <br></br>
       <Pinned />
     </>
   )
