76b75bb3236d1a517d77aaa7a8f93d88b11fdf0a
Clyde D'Souza <clyd94@gmail.com>
2023-06-17 15:06:36 +1200

    Temporarily log the node env site is build in

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,6 +2,7 @@ import Mainstage from './Components/Mainstage/Mainstage'
 import Sidebar from './Components/Sidebar/Sidebar'
 
 export function App() {
+  console.log(process.env.NODE_ENV)
   return (
     <>
       <Sidebar />
