9e2ee14deec1c573c1d523614bcd1fe444ddc327
Clyde D'Souza <clyd94@gmail.com>
2023-06-25 03:21:03 +1200

    Test react snap

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { hydrate, render } from 'react-dom'
 import { App } from './App'
 import './index.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const rootElement = document.getElementById('root')!
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement)
+} else {
+  render(<App />, rootElement)
+}
