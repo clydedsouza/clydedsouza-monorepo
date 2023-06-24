fe58750c627a7e3618e4c967fe484ed2a2a56b12
Clyde D'Souza <clyd94@gmail.com>
2023-06-25 03:28:50 +1200

    Fix non-null assertion

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -2,8 +2,8 @@ import { hydrate, render } from 'react-dom'
 import { App } from './App'
 import './index.scss'
 
-const rootElement = document.getElementById('root')!
-if (rootElement.hasChildNodes()) {
+const rootElement = document.getElementById('root')
+if (rootElement && rootElement.hasChildNodes()) {
   hydrate(<App />, rootElement)
 } else {
   render(<App />, rootElement)
