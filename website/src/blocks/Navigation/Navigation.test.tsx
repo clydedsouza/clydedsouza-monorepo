d5b4d1d6cf6b88b6d459760af0189624a7686cf7
Clyde D'Souza <clyd94@gmail.com>
2024-07-28 21:31:11 +1200

    Add nav item tests

--- website/src/blocks/Navigation/Navigation.test.tsx
+++ website/src/blocks/Navigation/Navigation.test.tsx
@@ -1,9 +1,38 @@
-import { render } from "@testing-library/react";
+import * as analyticsModule from "@/api/Analytics";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Navigation from "./Navigation";
+import { PageTypes } from "./PageTypes";
+
+jest.mock("../../api/Analytics");
 
 describe("Navigation", () => {
-  it("should render navigation", async () => {
+  it("should render navigation", () => {
     render(<Navigation />);
     expect(document.body).toMatchSnapshot();
+  });
+
+  it("should send analytics event when nav item is clicked", async () => {
+    const sendNavigationClickedEventMock = jest
+      .spyOn(analyticsModule, "sendNavigationClickedEvent")
+      .mockImplementation(jest.fn());
+    const user = userEvent.setup();
+
+    render(<Navigation />);
+
+    await user.click(screen.getByRole("link", { name: /Portfolio/i }));
+    expect(sendNavigationClickedEventMock).toHaveBeenCalledWith(
+      PageTypes.Portfolio
+    );
+  });
+
+  it("should set active class when nav item is clicked", async () => {
+    const user = userEvent.setup();
+
+    render(<Navigation />);
+
+    const platformsLink = screen.getByRole("link", { name: /Platforms/i });
+    await user.click(platformsLink);
+    expect(platformsLink).toHaveClass("active");
   });
 });
