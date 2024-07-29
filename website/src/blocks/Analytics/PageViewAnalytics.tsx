fa13e81a316d3a6539ca04fce15762df2322873d
Clyde D'Souza <clyd94@gmail.com>
2024-07-30 00:04:46 +1200

    Update exhaustive deps

--- website/src/blocks/Analytics/PageViewAnalytics.tsx
+++ website/src/blocks/Analytics/PageViewAnalytics.tsx
@@ -7,6 +7,6 @@ import { PageTypes } from "../Navigation/PageTypes";
 export const PageViewAnalytics: React.FC<{ page: PageTypes }> = ({ page }) => {
   useEffect(() => {
     sendPageViewEvent(page);
-  }, []);
+  }, [page]);
   return <></>;
 };
