b7c94ac1ef17807067a269ef1a07b00334cf5e63
Clyde D'Souza <clyd94@gmail.com>
2025-08-24 18:20:55 +1200

    Update page url logic

--- blog/components/SocialShare/SocialShare.tsx
+++ blog/components/SocialShare/SocialShare.tsx
@@ -1,4 +1,3 @@
-import siteMetadata from '@/data/siteMetadata'
 import {
   EmailIcon,
   EmailShareButton,
@@ -15,46 +14,40 @@ import {
   WhatsappIcon,
   WhatsappShareButton,
 } from 'next-share'
-
-const getCurrentUrl = () => {
-  if (typeof window !== 'undefined') {
-    return window.location.href
-  }
-  return siteMetadata.siteUrl
-}
+import { useEffect, useState } from 'react'
 
 export const SocialShare = ({ title, summary }: { title: string; summary?: string }) => {
-  const pathname = getCurrentUrl()
+  const [pageUrl, setPageUrl] = useState('')
+
+  useEffect(() => {
+    setPageUrl(window.location.href)
+  }, [window.location.href])
+
   return (
     <div className="my-6">
       <h2 className="mb-2 text-xs font-bold tracking-wide text-gray-800 uppercase dark:text-gray-400">
         Share this page
       </h2>
       <div className="flex gap-1">
-        <EmailShareButton url={pathname} subject={title} body={summary}>
+        <EmailShareButton url={pageUrl} subject={title} body={summary}>
           <EmailIcon size={32} round />
         </EmailShareButton>
-        <LinkedinShareButton url={pathname}>
+        <LinkedinShareButton url={pageUrl}>
           <LinkedinIcon size={32} round />
         </LinkedinShareButton>
-        <FacebookShareButton
-          url={pathname}
-          quote={title}
-          title={title}
-          hashtag={'#clydedsouzablog'}
-        >
+        <FacebookShareButton url={pageUrl} quote={title} title={title} hashtag={'#clydedsouzablog'}>
           <FacebookIcon size={32} round />
         </FacebookShareButton>
-        <RedditShareButton url={pathname} title={title}>
+        <RedditShareButton url={pageUrl} title={title}>
           <RedditIcon size={32} round />
         </RedditShareButton>
-        <TwitterShareButton url={pathname} title={title}>
+        <TwitterShareButton url={pageUrl} title={title}>
           <TwitterIcon size={32} round />
         </TwitterShareButton>
-        <TelegramShareButton url={pathname} title={title}>
+        <TelegramShareButton url={pageUrl} title={title}>
           <TelegramIcon size={32} round />
         </TelegramShareButton>
-        <WhatsappShareButton url={pathname} title={title} separator=":: ">
+        <WhatsappShareButton url={pageUrl} title={title} separator=":: ">
           <WhatsappIcon size={32} round />
         </WhatsappShareButton>
       </div>
--- blog/lib/mixpanel.ts
+++ blog/lib/mixpanel.ts
@@ -2,8 +2,7 @@ import mixpanel from 'mixpanel-browser/dist/mixpanel.cjs'
 
 const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN
 
-export const initMixpanel = () => {
-  console.log(MIXPANEL_TOKEN)
+export const initMixpanel = () => { 
   if (!MIXPANEL_TOKEN) {
     console.warn('Mixpanel token is missing! Check your .env file.')
     return
