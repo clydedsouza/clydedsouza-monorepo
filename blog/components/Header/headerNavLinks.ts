7b1af1931cc9117b48d9ee70dcf3c15b90b2ab62
Clyde D'Souza <clyd94@gmail.com>
2025-09-18 21:25:20 +1200

    Patch fixes to the blog website

--- blog/components/Header/headerNavLinks.ts
+++ blog/components/Header/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/posts', title: 'All posts' },
-  { href: '/lists', title: 'Reading lists' },
-  { href: '/tags', title: 'Tags' },
+  // { href: '/lists', title: 'Reading lists' },
+  // { href: '/tags', title: 'Tags' },
 ]
 
 export default headerNavLinks
