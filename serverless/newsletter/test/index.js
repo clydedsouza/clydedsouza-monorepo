7e34b97f065dbdc5947fb1ad46de1b9b2c7857ce
Clyde D'Souza <clyd94@gmail.com>
2025-09-21 02:32:35 +1200

    Lint test script

--- serverless/newsletter/test/index.js
+++ serverless/newsletter/test/index.js
@@ -3,29 +3,29 @@
 
 const testLocal = true;
 
-const url = testLocal ? "http://127.0.0.1:8787/" : "https://newsletter.clydedz.workers.dev/"
+const url = testLocal ? 'http://127.0.0.1:8787/' : 'https://newsletter.clydedz.workers.dev/';
 
 const postData = {
-  name: "John Doe",
-  email: "john.doe@example.com", 
+	name: 'John Doe',
+	email: 'john.doe@example.com',
 };
 
 fetch(url, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(postData) 
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	body: JSON.stringify(postData),
 })
-.then(response => {
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  return response.json();  
-})
-.then(data => {
-  console.log('Success:', data);  
-})
-.catch(error => {
-  console.error('Error:', error);  
-});
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		return response.json();
+	})
+	.then((data) => {
+		console.log('Success:', data);
+	})
+	.catch((error) => {
+		console.error('Error:', error);
+	});
