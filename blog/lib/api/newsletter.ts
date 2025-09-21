ee6e6db94bee2349387290ae8627b9e26806339c
Clyde D'Souza <clyd94@gmail.com>
2025-09-21 17:14:09 +1200

    Update blog site to call newsletter worker

--- blog/lib/api/newsletter.ts
+++ blog/lib/api/newsletter.ts
@@ -1,12 +1,14 @@
-export interface INewsletterPayload {
+interface INewsletterPayload {
   firstName: string
   email: string
 }
 
 export async function addSubscriber({ firstName, email }: INewsletterPayload) {
-  return await fetch('/api/newsletter', {
+  const newsletterApiUrl = process.env.NEXT_PUBLIC_NEWSLETTER_API_URL ?? 'http://127.0.0.1:8787/'
+
+  return await fetch(newsletterApiUrl, {
     method: 'POST',
-    body: JSON.stringify({ email, firstName }),
+    body: JSON.stringify({ name: firstName, email }),
     headers: { 'Content-Type': 'application/json' },
   })
 }
