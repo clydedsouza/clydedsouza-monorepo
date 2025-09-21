29b1f47cd3c0ca73a4943713f2363c8dc635caea
Clyde D'Souza <clyd94@gmail.com>
2025-09-21 14:19:00 +1200

    Add CORS to cloudflare worker

--- serverless/newsletter/src/index.ts
+++ serverless/newsletter/src/index.ts
@@ -3,8 +3,35 @@ import { INewsletter } from './types';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		if (request.method === 'OPTIONS') {
+			return new Response(null, {
+				headers: {
+					'Access-Control-Allow-Origin': env.CORS_ALLOW_ORIGINS,
+					'Access-Control-Allow-Methods': 'POST, OPTIONS',
+					'Access-Control-Allow-Headers': 'Content-Type',
+				},
+			});
+		}
+
+		const incomingOrigin = request.headers.get('Origin');
+
+		if (incomingOrigin && incomingOrigin !== env.CORS_ALLOW_ORIGINS) {
+			return new Response(JSON.stringify({ message: 'Forbidden' }), {
+				status: 403,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
 		if (request.method !== 'POST') {
-			return new Response(JSON.stringify({ message: 'Method not allowed' }), { status: 405 });
+			return new Response(JSON.stringify({ message: 'Method not allowed' }), {
+				status: 405,
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': env.CORS_ALLOW_ORIGINS,
+				},
+			});
 		}
 
 		const textBody = await request.text();
@@ -15,13 +42,19 @@ export default {
 		if (!response) {
 			return new Response(JSON.stringify({ message: "Subscriber couldn't be added to the newsletter" }), {
 				status: 500,
-				headers: { 'Content-Type': 'application/json' },
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': env.CORS_ALLOW_ORIGINS,
+				},
 			});
 		}
 
 		return new Response(JSON.stringify({ message: 'Subscriber  added to the newsletter' }), {
 			status: 200,
-			headers: { 'Content-Type': 'application/json' },
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': env.CORS_ALLOW_ORIGINS,
+			},
 		});
 	},
 } satisfies ExportedHandler<Env>;
