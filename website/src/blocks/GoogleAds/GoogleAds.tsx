e807707f401e806dcbbf82c67a84b82c8b042f29
Clyde D'Souza <clyd94@gmail.com>
2025-09-25 23:11:17 +1200

    Load google ad on every app route change

--- website/src/blocks/GoogleAds/GoogleAds.tsx
+++ website/src/blocks/GoogleAds/GoogleAds.tsx
@@ -1,13 +1,15 @@
 import { AdUnit } from "next-google-adsense";
 import "./GoogleAds.scss";
+import { usePathname } from "next/navigation";
 
 export const GoogleAds = () => {
+  const path = usePathname();
   const isProduction = process.env.NODE_ENV === "production";
   const optionalAttributes = isProduction
     ? {}
     : { className: "google-ads-container-dev" };
   return (
-    <div className="google-ads">
+    <div className="google-ads" key={path.toString()}>
       <div {...optionalAttributes}>
         <AdUnit slotId="7735157113" layout="in-article" />
       </div>
