38f82851524033ff78218916fd6f08ee797d0544
Clyde D'Souza <clyd94@gmail.com>
2025-09-26 18:35:18 +1200

    Update next-google-adsense to beta version

--- blog/components/GoogleAds/GoogleAds.tsx
+++ blog/components/GoogleAds/GoogleAds.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { AdUnit } from 'next-google-adsense'
-import { usePathname } from 'next/navigation'
 
 interface IGoogleAds {
   adLayout?: 'Square' | 'Responsive'
@@ -10,9 +9,6 @@ interface IGoogleAds {
 export const GoogleAds = ({ adLayout = 'Square' }: IGoogleAds) => {
   const isProduction = process.env.NODE_ENV === 'production'
   const optionalAttributes = isProduction ? {} : { className: 'border-2 border-indigo-500' }
-
-  const path = usePathname()
-  const keyAttributes = isProduction ? { key: path.toString() } : {}
 
   const containerWidth = adLayout === 'Square' ? 'w-[302px]' : 'w-full'
 
@@ -24,7 +20,7 @@ export const GoogleAds = ({ adLayout = 'Square' }: IGoogleAds) => {
     )
 
   return (
-    <div className="flex justify-center" {...keyAttributes}>
+    <div className="flex justify-center">
       <div className={`my-4 ${containerWidth}`}>
         <div {...optionalAttributes}>{advt}</div>
         <div className="mx-auto px-0 py-1 text-right text-xs font-medium text-black/50 no-underline">
